4fa44fa2916d479c3328cb16bcda9694b58cdbc7
Nathan Arthur <nathan@nathanarthur.com>
2025-05-15 09:00:01 -0400

    add tiptap

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { Editor } from '../components/editor';
+import { EditorProvider } from '../context/EditorContext';
+
 export default function Home() {
   return (
     <main className="min-h-screen p-4">
-      <h1>BeePad</h1>
-      {/* Editor will be mounted here */}
+      <h1 className="text-3xl font-bold text-center mb-8">BeePad</h1>
+      <EditorProvider>
+        <Editor />
+      </EditorProvider>
     </main>
   );
 }
